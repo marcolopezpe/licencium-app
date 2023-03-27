5b54dbc8d7849b62c53945c185b9beba221ee169
Marco López <marcolopezpe@outlook.com>
2023-03-27 09:23:17 -0500

    add api gateway

--- api-gateway/src/main/java/pe/marcolopez/apps/licencium/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/pe/marcolopez/apps/licencium/apigateway/ApiGatewayApplication.java
@@ -3,10 +3,11 @@ package pe.marcolopez.apps.licencium.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
