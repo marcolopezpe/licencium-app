e56bbb5cddc0c4f883086f1273b56dd7984db8e0
Marco López <marcolopezpe@outlook.com>
2023-03-30 09:02:34 -0500

    update properties

--- auditoria-service/src/main/java/pe/marcolopez/apps/licencium/auditoriaservice/config/KafkaConfig.java
+++ auditoria-service/src/main/java/pe/marcolopez/apps/licencium/auditoriaservice/config/KafkaConfig.java
@@ -1,17 +1,13 @@
-package pe.marcolopez.apps.licencium.usuarioservice.config;
+package pe.marcolopez.apps.licencium.auditoriaservice.config;
 
 import com.google.gson.Gson;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-public class KafkaTopicConfig {
-
-    @Value("${spring.kafka.topic.name}")
-    private String topicName;
+public class KafkaConfig {
 
     @Bean
     public NewTopic topic() {
