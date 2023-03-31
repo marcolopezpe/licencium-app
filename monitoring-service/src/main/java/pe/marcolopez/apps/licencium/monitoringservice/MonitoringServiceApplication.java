b5e946379e5cafddd94eed1156a9679476fcb59a
Marco López <marcolopezpe@outlook.com>
2023-03-31 15:43:25 -0500

    monitoring service and update properties

---
+++ monitoring-service/src/main/java/pe/marcolopez/apps/licencium/monitoringservice/MonitoringServiceApplication.java
@@ -0,0 +1,15 @@
+package pe.marcolopez.apps.licencium.monitoringservice;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class MonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ monitoring-service/src/test/java/pe/marcolopez/apps/licencium/monitoringservice/MonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.marcolopez.apps.licencium.monitoringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
